48daf286a162afe72ddcb0bca791916cc071d1ea
Ranjan kumar <ranjankumar5673@gmail.com>
2024-06-13 11:04:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Tea from "./tea";
+function App() {
+  const username="with Ranjan"
+  return (
+    <>
+    <Tea/>
+    <h1>tea and react {username}</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App;
---
+++ src/tea.jsx
@@ -0,0 +1,6 @@
+function Tea(){
+  return (
+    <h3>tea is ready</h3>
+  )
+}
+export default Tea;
